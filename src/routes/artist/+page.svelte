db20d2ff04b15d5c73569715d36240c8cb88de43
sha5b <ned.tabulov@gmail.com>
2024-12-21 18:26:42 +0100

    Update +page.svelte

--- src/routes/artist/+page.svelte
+++ src/routes/artist/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     import ArtistSlider from '$lib/components/slider/ArtistSlider.svelte';
-    import LatestArtistExhibition from '$lib/components/LatestArtistExhibition.svelte';
     import PageSEO from '$lib/components/base/PageSEO.svelte';
     import { page } from '$app/stores';
     
@@ -20,28 +19,6 @@
                 Each artist brings a unique perspective to the digital art landscape through virtual exhibitions, 
                 NFT collections, and experimental media.
             </p>
-        </section>
-
-        <section class="space-y-md">
-            <div class="grid grid-cols-1 gap-xl md:grid-cols-2">
-                {#each artists as artist (artist.id)}
-                    {#if getLatestVirtualExhibitionForArtist(exhibitions, artist.id) !== undefined}
-                        <div class="space-y-lg">
-                            <a href={`/artist/${artist.slug}`}>
-                                <h3 class="h3">
-                                    {artist.title.rendered}
-                                </h3>
-                            </a>
-                            <div class="overflow-hidden">
-                                <ArtistSlider {products} artistName={artist.title.rendered} />
-                            </div>
-                            <p class="text-base">
-                                {artist.acf?.description || `Artist based in ${artist.acf?.location || 'various locations'}`}
-                            </p>
-                        </div>
-                    {/if}
-                {/each}
-            </div>
         </section>
     </div>
 </div>
